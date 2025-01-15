e4322d962233e603a1250edbaf04dceaff9885a6
Carlos Nogueira <carlos.nogueira@datadoghq.com>
2025-01-15 19:00:41 +0000

    Fix example project & minor react-native version upgrade
    - Upgrade react-native to version 0.74.6
    - Update project dependencies
    - Fix metro & babel configs
    - Upgrade gradle version
    - Increase minSdkVersion & ndkVersion
    - Update 'WixApp' component to access 'Navigation' object from
    '@datadog/mobile-react-native-navigation'

--- example/babel.config.js
+++ example/babel.config.js
@@ -4,29 +4,29 @@ const pakNavigation = require('../packages/react-navigation/package.json');
 const pakNativeNavigation = require('../packages/react-native-navigation/package.json');
 
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          [pakCore.name]: path.join(
-            __dirname,
-            '../packages/core',
-            pakCore.source,
-          ),
-          [pakNavigation.name]: path.join(
-            __dirname,
-            '../packages/react-navigation',
-            pakNavigation.source,
-          ),
-          [pakNativeNavigation.name]: path.join(
-            __dirname,
-            '../packages/react-native-navigation',
-            pakNativeNavigation.source,
-          ),
-        },
-      },
-    ],
-  ],
+    presets: ['module:@react-native/babel-preset']
+    // plugins: [
+    //   [
+    //     'module-resolver',
+    //     {
+    //       alias: {
+    //         [pakCore.name]: path.join(
+    //           __dirname,
+    //           '../packages/core',
+    //           pakCore.source,
+    //         ),
+    //         [pakNavigation.name]: path.join(
+    //           __dirname,
+    //           '../packages/react-navigation',
+    //           pakNavigation.source,
+    //         ),
+    //         [pakNativeNavigation.name]: path.join(
+    //           __dirname,
+    //           '../packages/react-native-navigation',
+    //           pakNativeNavigation.source,
+    //         ),
+    //       },
+    //     },
+    //   ],
+    // ],
 };
